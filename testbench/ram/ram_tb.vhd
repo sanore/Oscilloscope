efc6b6c09b1b08719073817ee699208592250f7a
Erik Löffler <erik.loeffler@ost.ch>
2022-11-25 11:02:10 +0100

    added RAM component to testbench

--- testbench/ram/ram_tb.vhd
+++ testbench/ram/ram_tb.vhd
@@ -1,3 +1,15 @@
+-- # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+-- #                                                                     #
+-- # Oscilloscope                                                        #
+-- # Miniproject Digital Microelectronics (Fall Semester 2022)           #
+-- # OST Rapperswil-Jona                                                 #
+-- #                                                                     #
+-- # Group 7:   Pelé Constam                                             #
+-- #            Sandro Pedrett                                           #
+-- #            Erik Löffler                                             #
+-- #                                                                     #
+-- # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -6,7 +18,26 @@ entity ram_tb is
 end entity ram_tb;
 
 architecture RTL of ram_tb is
+    ----------------------------------------------------------------------------
+    -- component
+    ----------------------------------------------------------------------------
     
+component ram is
+    generic(ADDR_WIDTH : integer := 13;
+            DATA_WIDTH : integer := 16
+           );
+    port(
+        clk           : in  std_logic;
+        rst           : in  std_logic;
+        write_address : in  std_ulogic_vector(ADDR_WIDTH - 1 downto 0);
+        write_data    : in  std_ulogic_vector(DATA_WIDTH - 1 downto 0);
+        write_en      : in  std_ulogic;
+        read_address  : in std_ulogic_vector(ADDR_WIDTH - 1 downto 0);
+        read_data     : out std_ulogic_vector(DATA_WIDTH - 1 downto 0);
+        read_en       : in  std_ulogic
+    );
+end component;
+
 begin
 
 end architecture RTL;
