59eac10dcd118bbfcc0ba60361b727476bad7ead
Erik Löffler <erik.loeffler@ost.ch>
2022-11-29 10:59:55 +0100

    register and sample logic for trigger

--- modules/trigger/trigger.vhd
+++ modules/trigger/trigger.vhd
@@ -32,7 +32,31 @@ entity trigger is
 end entity trigger;
 
 architecture RTL of trigger is
+    -- sample buffers
+    signal current_sample: std_ulogic_vector(11 downto 0);
+    signal previous_sample : std_ulogic_vector(11 downto 0);
+
+    -- states (required to not trigger immediately when initial sample is non zero)
+    type trigger_state_type is (state_stopped, state_starting, state_armed);
+    signal current_trigger_state: trigger_state_type := state_stopped;
+    signal next_trigger_state: trigger_state_type := state_stopped;
 
 begin
+
+    -- state and sample registers
+    -- sample is taken at the rising edge of the clock
+    reg_process: process(clk)
+    begin
+        if rising_edge(clk) then
+            if enable = '1' then
+                current_trigger_state <= next_trigger_state;
+                previous_sample <= current_sample;
+                current_sample <= adc_val;
+            else
+                current_trigger_state <= state_stopped;
+                current_sample <= (others => '0');
+            end if;
+        end if;
+    end process reg_process;
 
 end architecture RTL;
