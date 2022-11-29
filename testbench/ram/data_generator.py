886d73f91ee0a7141c3698e792849ef0ebcc434a
Erik Löffler <erik.loeffler@ost.ch>
2022-11-29 10:26:05 +0100

    randomized RAM access in test data

--- testbench/ram/data_generator.py
+++ testbench/ram/data_generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 RAM_SIZE = 2**13
 DATA_BITS = 16
@@ -7,7 +8,10 @@ data = np.random.randint(0, 2**DATA_BITS, size=RAM_SIZE)
 
 file_content = []
 for i, val in enumerate(data):
-    file_content.append(f"{i};{val:04x}\n")
+    file_content.append(f"{i:04x};{val:04x}\n")
+
+# shuffle lines for random RAM access
+random.shuffle(file_content)
 
 with open('ram_data.csv', 'w+') as f:
     f.writelines(file_content)
