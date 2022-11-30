096db4135c23e5892057e9235ffb5cfeeb934df4
Erik Löffler <100943759+erik-loeffler@users.noreply.github.com>
2022-11-30 21:30:23 +0100

    added code to generate rising edge test data

---
+++ testbench/trigger/test_waveform_generator.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# wave shapes in order
+#   1. sine
+#   2. sawtooth
+#   3. rectangular
+#   4. random (uniformly distributed)
+SAMPLES_PER_WAVE_PERIOD = 1000
+PERIODS_PER_WAVE_SHAPE = 2
+MAX_ADC_VALUE = 2**12 - 1
+
+TRIGGER_VALUE = 2500
+
+FILE_NAMES = {"rising_edge": "rising_edge_test.csv",
+              "falling_edge": "falling_edge_test.csv",
+              "both_edges": "both_edges_test.csv"}
+
+# generate test data
+test_data = np.ndarray((4*SAMPLES_PER_WAVE_PERIOD*PERIODS_PER_WAVE_SHAPE,),
+        dtype=np.uint16)
+
+# generate sine wave
+timebase_sin = np.linspace(0, 2*np.pi,
+                           SAMPLES_PER_WAVE_PERIOD)
+
+sin_data = np.round(MAX_ADC_VALUE//2*np.sin(timebase_sin) + MAX_ADC_VALUE//2)
+# store sine wave
+for i in range(PERIODS_PER_WAVE_SHAPE):
+    test_data[i*SAMPLES_PER_WAVE_PERIOD:(i + 1)*SAMPLES_PER_WAVE_PERIOD] = sin_data
+
+# generate sawtooth
+sawtooth_values = np.ndarray((SAMPLES_PER_WAVE_PERIOD,), dtype=np.uint16)
+dy_over_dx = MAX_ADC_VALUE / SAMPLES_PER_WAVE_PERIOD
+for i in range(SAMPLES_PER_WAVE_PERIOD):
+    sawtooth_values[i] = i*dy_over_dx
+# store sawtooth
+for i in range(PERIODS_PER_WAVE_SHAPE):
+    test_data[SAMPLES_PER_WAVE_PERIOD*PERIODS_PER_WAVE_SHAPE + 
+              i*SAMPLES_PER_WAVE_PERIOD:
+                  SAMPLES_PER_WAVE_PERIOD*PERIODS_PER_WAVE_SHAPE + 
+                  (i + 1)*SAMPLES_PER_WAVE_PERIOD] = sawtooth_values
+
+# generate rectangular
+mid_point = SAMPLES_PER_WAVE_PERIOD//2
+rect_data = np.ndarray((SAMPLES_PER_WAVE_PERIOD,), dtype=np.uint16)
+for i in range(SAMPLES_PER_WAVE_PERIOD):
+    if i < mid_point:
+        rect_data[i] = 0
+    else:
+        rect_data[i] = MAX_ADC_VALUE
+# store rectangular
+for i in range(PERIODS_PER_WAVE_SHAPE):
+    test_data[2*SAMPLES_PER_WAVE_PERIOD*PERIODS_PER_WAVE_SHAPE + 
+              i*SAMPLES_PER_WAVE_PERIOD:
+                  2*SAMPLES_PER_WAVE_PERIOD*PERIODS_PER_WAVE_SHAPE + 
+                  (i + 1)*SAMPLES_PER_WAVE_PERIOD] = rect_data
+
+# generate random pattern
+test_data[3*SAMPLES_PER_WAVE_PERIOD*PERIODS_PER_WAVE_SHAPE:] = \
+    np.round(np.random.uniform(low=0, high=MAX_ADC_VALUE,
+                             size=SAMPLES_PER_WAVE_PERIOD*PERIODS_PER_WAVE_SHAPE))
+
+# determine rising edge trigger and save to file
+output_lines = ["0000\n", # trigger mode
+                "0001\n", # trigger sel
+                "{TRIGGER_VALUE:012b}\n"] # trigger threshold
+output_lines.append(f"{test_data[0]:012b};0\n")
+for i in range(1, test_data.shape[0]):
+    if test_data[i] >= TRIGGER_VALUE and test_data[i - 1] <= TRIGGER_VALUE:
+        output_lines.append(f"{test_data[i]:012b};1\n")
+    else:
+        output_lines.append(f"{test_data[i]:012b};0\n")
+
+# store file
+with open(FILE_NAMES['rising_edge'], 'w+') as f:
+    f.writelines(output_lines)
+    
+
+plotx = np.arange(test_data.shape[0])
+plt.plot(plotx, test_data)
