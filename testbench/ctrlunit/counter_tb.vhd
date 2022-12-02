4a09006bbf246c7f167a9ad6a3041bf813e093a3
Pelé <pele.constam@gmail.com>
2022-12-02 08:45:58 +0100

    counter_tb update

--- testbench/ctrlunit/counter_tb.vhd
+++ testbench/ctrlunit/counter_tb.vhd
@@ -29,15 +29,34 @@ begin
 
     dut_clk <= not dut_clk after 5 ns;
     reset   <= '1', '0' after 50 ns;
-    enable  <= '0', '1' after 50 ns;
+    enable  <= '0', '1' after 60 ns;
     dut_proc : process is
     begin
-        wait for 55 ns;
-        for i in 0 to 10 loop
+        reset <= '1';
+        enable <= '0';
+        wait for 50 ns;
+        assert(dut_cnt = std_ulogic_vector(to_unsigned(0, dut_cnt'length))) report "unexpected counter value. Expected 0, got " & to_string(dut_cnt) severity error;
+        reset <= '0';
+        wait for 10 ns;
+        assert(dut_cnt = std_ulogic_vector(to_unsigned(0, dut_cnt'length))) report "unexpected counter value. Expected 0, got " & to_string(dut_cnt) severity error;
+        enable <= '1';
+        for i in 0 to 2**13 -1 loop
             wait for 5 ns;
-            assert (dut_cnt = std_ulogic_vector(to_unsigned(i, dut_cnt'length))) report "unexpected counter value" severity error;
-            wait for 99 * 10 ns + 5 ns;
+            assert (dut_cnt = std_ulogic_vector(to_unsigned(i, dut_cnt'length))) report "unexpected counter value. Expected " & integer'image(i) & ", got " & to_string(dut_cnt) severity error;
+            wait for 995 ns;
         end loop;
+        
+        wait for 5 ns;
+        assert(dut_cnt = std_ulogic_vector(to_unsigned(0, dut_cnt'length))) report "unexpected counter value. Expected 0, got " & to_string(dut_cnt) severity error;
+        wait for 10 ns;
+        enable <= '0';
+        -- counter is stopped
+        wait for 50 ns;
+        assert(dut_cnt = std_ulogic_vector(to_unsigned(1, dut_cnt'length))) report "unexpected counter value. Expected 1, got " & to_string(dut_cnt) severity error;
+
+        reset <= '1';
+        wait for 10 ns;
+        assert(dut_cnt = std_ulogic_vector(to_unsigned(0, dut_cnt'length))) report "unexpected counter value. Expected 0, got " & to_string(dut_cnt) severity error;
         
         assert false report "simulation ended" severity error;
     end process dut_proc;
